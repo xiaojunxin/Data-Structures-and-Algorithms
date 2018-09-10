48bf00d248a739eae049bb9e679373b723f03542
xiaojunxin <33772076+xiaojunxin@users.noreply.github.com>
2018-09-10 09:40:08 +0800

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+using std::istream;
+
+/*void func(istream &is)
+{
+    std::string buf;
+    while (is >> buf)
+        std::cout << buf << std::endl;
+    is.clear();
+    //return is;
+}
+*/
+int main()
+{
+	int a;
+     //func(std::cin);
+    //std::cout << is.rdstate() << std::endl;
+	//istream &is;
+	//is=std::cin£»
+	//ostream &os=std::cout;
+	std::cin>>a;
+	std::cout<<a;
+    return 0;
+}
